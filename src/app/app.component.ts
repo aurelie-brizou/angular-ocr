06151537cd414a6ce3974f4d6942d68d2be3c021
Aurelie <brizouaurelie@gmail.com>
2018-08-03 08:42:41 +0200

    premiere partie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { resolve } from 'url';
+import { AppareilService } from './service/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app-angular';
+export class AppComponent implements OnInit {
+  // création d'une valeur globale donc ds app-component
+  // la condition vérifie si l'utilisateur est authentifié 
+  isAuth = false;
+
+  lastUpdate = new Date();
+
+  // def d'une propriété qui est vouée à être async
+  lastUpdatePromise = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+ 
+
+  appareils:any[];
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+
+  // indique que l'array appareils sera égale au tableau du service
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
+  // le component appelle les méthodes du service
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  onEteindre() {
+    this.appareilService.switchOffAll();
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
+import { AppareilService } from './service/appareil.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
